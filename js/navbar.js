29fa1af45866373bbc955fd0fe509efa694d3e5c
culrizz <culrizz18@gmail.com>
2026-07-31 00:32:44 +0300

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,55 @@
+/* ==========================================
+   NAVBAR
+========================================== */
 
+function initializeNavbar(){
+
+    highlightCurrentPage();
+
+    navbarScrollEffect();
+
+}
+
+function highlightCurrentPage(){
+
+    const currentPage = window.location.pathname.split("/").pop();
+
+    const links = document.querySelectorAll(".nav-links a");
+
+    links.forEach(link=>{
+
+        const href = link.getAttribute("href");
+
+        if(href===currentPage){
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+}
+
+function navbarScrollEffect(){
+
+    const header = document.querySelector("header");
+
+    if(!header) return;
+
+    window.addEventListener("scroll",()=>{
+
+        if(window.scrollY>40){
+
+            header.classList.add("scrolled");
+
+        }
+
+        else{
+
+            header.classList.remove("scrolled");
+
+        }
+
+    });
+
+}
